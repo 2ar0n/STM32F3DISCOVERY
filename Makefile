2b335992de26b88dd637b7810656c5f1dd645688
Aaron Lelouvier <aaron.lelouvier@protonmail.com>
2022-09-08 22:10:01 +0200

    Created based project with sample programm.

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
-DEVICE = STM32F301x8
+DEVICE = STM32F303xB
 FLASH  = 0x08000000
 
 USE_ST_CMSIS = true
 
+# use system toolchain
+TOOLCHAIN_PATH =
+
 # Include the main makefile
 include ./STM32-base/make/common.mk
+
+flash: all
+	openocd -f interface/stlink-v2.cfg -f target/stm32f3x.cfg -c "program $(ELF_FILE_PATH) verify reset exit"
--- src/main.c
+++ src/main.c
@@ -7,22 +7,49 @@ static void delay (unsigned int time) {
 }
 
 int main (void) {
-    // Turn on the GPIOC peripheral
-    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    // Turn on the GPIOE peripheral
+    RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
 
-    // Put pin 13 in general purpose mode
-    GPIOC->MODER  |= GPIO_MODER_MODER13_0;
+    // Put pins in general purpose mode
+    GPIOE->MODER  |= GPIO_MODER_MODER8_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER9_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER10_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER11_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER12_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER13_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER14_0;
+    GPIOE->MODER  |= GPIO_MODER_MODER15_0;
+
+    unsigned int waitTime = 300; 
 
     while (1) {
-        // Reset the state of pin 13 to output low
-        GPIOC->BSRR = GPIO_BSRR_BR_13;
+        // Reset the state of pin 8 to output low
+        GPIOE->BSRR = GPIO_BSRR_BR_8;
+        delay(waitTime);
+        // Set the state of pin 8 to output high
+        GPIOE->BSRR = GPIO_BSRR_BS_8;
+        delay(waitTime);
 
-        delay(500);
+        // Reset the state of pin 8 to output low
+        GPIOE->BSRR = GPIO_BSRR_BR_9;
+        delay(waitTime);
+        // Set the state of pin 8 to output high
+        GPIOE->BSRR = GPIO_BSRR_BS_9;
+        delay(waitTime);
 
-        // Set the state of pin 13 to output high
-        GPIOC->BSRR = GPIO_BSRR_BS_13;
+        // Reset the state of pin 8 to output low
+        GPIOE->BSRR = GPIO_BSRR_BR_10;
+        delay(waitTime);
+        // Set the state of pin 8 to output high
+        GPIOE->BSRR = GPIO_BSRR_BS_10;
+        delay(waitTime);
 
-        delay(500);
+        // Reset the state of pin 8 to output low
+        GPIOE->BSRR = GPIO_BSRR_BR_11;
+        delay(waitTime);
+        // Set the state of pin 8 to output high
+        GPIOE->BSRR = GPIO_BSRR_BS_11;
+        delay(waitTime);
     }
 
     // Return 0 to satisfy compiler
